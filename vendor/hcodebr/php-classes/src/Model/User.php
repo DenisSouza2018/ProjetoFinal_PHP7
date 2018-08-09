5b9109093cb5763eb42db471f6fbf639a7d3a74a
Unknown <denis.souza.rosa@gmail.com>
2018-08-09 15:37:35 -0300

    Configurando Admin - Login e trabalhando com Rotas

---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,78 @@
+<?php 
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+
+class User extends Model{
+
+	const SESSION = "User";
+
+	public static function login($login, $passaword)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN",array(
+			":LOGIN"=>$login
+		));
+		if(count($results)===0)
+		{
+			throw new \Exception("Senha Invalida ou Usuario inexistente");
+		}
+
+		$data = $results[0];
+
+		if(password_verify($passaword, $data["despassword"]) === true)
+		{
+			$user = new User();
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+
+		}else{
+			throw new \Exception("Senha Invalida ou Usuario inexistente");
+		}
+
+	} 
+
+	public static function verifyLogin($inadmin = true)
+	{
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+
+		){
+			header("Location: /admin/login");
+			exit;
+		}
+
+
+
+	}
+
+	public static function logout()
+	{
+		$_SESSION[User::SESSION] = NULL;
+	}
+
+
+
+
+
+}
+
+
+
+ ?>
