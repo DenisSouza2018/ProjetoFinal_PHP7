a4b688b1d1bf525a17b56221e00cb608af6771b9
Denis <denis.souza.rosa@gmail.com>
2019-01-14 18:03:15 -0200

    Implementando pagina da minha conta do Usuario

--- site.php
+++ site.php
@@ -377,7 +377,7 @@ $app->post("/forgot/reset", function(){
 
 //**********************************************************
 $app->get("/profile", function(){
-	
+
 	User::verifyLogin(false);
 	$user = User::getFromSession();
 	$page = new Page();
@@ -390,17 +390,21 @@ $app->get("/profile", function(){
 
 //**********************************************************
 $app->post("/profile", function(){
+
 	User::verifyLogin(false);
+
 	if (!isset($_POST['desperson']) || $_POST['desperson'] === '') {
 		User::setError("Preencha o seu nome.");
 		header('Location: /profile');
 		exit;
 	}
+
 	if (!isset($_POST['desemail']) || $_POST['desemail'] === '') {
 		User::setError("Preencha o seu e-mail.");
 		header('Location: /profile');
 		exit;
 	}
+
 	$user = User::getFromSession();
 	if ($_POST['desemail'] !== $user->getdesemail()) {
 		if (User::checkLoginExists($_POST['desemail']) === true) {
@@ -409,14 +413,17 @@ $app->post("/profile", function(){
 			exit;
 		}
 	}
+
 	$_POST['inadmin'] = $user->getinadmin();
 	$_POST['despassword'] = $user->getdespassword();
 	$_POST['deslogin'] = $_POST['desemail'];
 	$user->setData($_POST);
 	$user->save();
+	
 	User::setSuccess("Dados alterados com sucesso!");
 	header('Location: /profile');
 	exit;
+
 });
 
 //**********************************************************
